7cc229a92e01fb660f34b3d51a1fa510de66cf1a
rorymurphy <ndmurph@gmail.com>
2014-04-13 00:18:22 -0700

    Added urls to list view for sorting and paging Adding initial work to create scripts to automate docker setup minor style tweak

--- fullstackweb/app/retailmenot/retailmenot/product_viewer/urls.py
+++ fullstackweb/app/retailmenot/retailmenot/product_viewer/urls.py
@@ -6,7 +6,9 @@ urlpatterns = patterns('',
     # ex: /product/
     url(r'^$', views.index, name='index'),
     url(r'^list/$', views.list, name='list'),
-    # ex: /polls/5/
+    url(r'^list/(?P<sort_field>\w+)/(?P<sort_dir>asc|desc)/$', views.list, name='list'),
+    url(r'^list/(?P<sort_field>\w+)/(?P<sort_dir>asc|desc)/(?P<page>\d+)/$', views.list, name='list'),
+    # ex: /product/5/
     url(r'^(?P<prod_id>\w+)/$', views.detail, name='detail'),
     # ex: /polls/5/results/
     #url(r'^(?P<poll_id>\d+)/results/$', views.results, name='results'),
--- fullstackweb/app/retailmenot/retailmenot/product_viewer/views.py
+++ fullstackweb/app/retailmenot/retailmenot/product_viewer/views.py
@@ -1,12 +1,14 @@
 from django.template import RequestContext, loader
 
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.core.serializers.json import DjangoJSONEncoder
 from django.core import serializers
 from django_statsd.clients import statsd
 
 from product_viewer.models import Product, Image
+
+PAGE_SIZE = 20
 
 def index(request):
     prods_by_title = Product.objects.order_by('page_title')
@@ -14,10 +16,16 @@ def index(request):
     statsd.incr('response.200')
     return render(request, 'product_viewer/index.html', context)
 
-def list(request):
-    prods_by_title = Product.objects.order_by('page_title')
+def list(request, sort_field = 'page_title', sort_dir = 'desc', page = 0):
+    page = int(page)
+    prods = Product.objects.order_by(sort_field)
+    if sort_dir == 'desc':
+        prods = prods.reverse()
+    
+    page_start = page * PAGE_SIZE
+    prods = prods[page_start : page_start + PAGE_SIZE]
     statsd.incr('response.200')
-    return HttpResponse(serializers.serialize('json', prods_by_title),
+    return HttpResponse(serializers.serialize('json', prods),
                         content_type="application/json")
 
 def detail(request):
